11b914c256143b06612f80e6bcddbd7cb9294a1f
okkartal <o_kartal@aof.anadolu.edu.tr>
2024-01-13 15:35:25 +0300

    added middlewares & filters

---
+++ src/middlewares/api-key.middleware.ts
@@ -0,0 +1,15 @@
+import { NestMiddleware } from "@nestjs/common";
+import { Request, Response, NextFunction } from "express";
+import { config } from "src/config/config";
+
+export class ApiKeyMiddleware implements NestMiddleware {
+    use(req: Request, res: Response, next: NextFunction){
+        const apiKey = req.headers['x-api-key'];
+
+        if(!apiKey || !config.apiKeys.includes(apiKey.toString())) {
+            return res.status(401).json({ message: 'Unathorized'});
+        }
+
+        next();
+    }
+}
