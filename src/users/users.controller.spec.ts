c37f7377a2e84d687a9bb9234e4c2d2a8148aba3
okkartal <o_kartal@aof.anadolu.edu.tr>
2024-01-14 13:58:12 +0400

    covered users test spec

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,12 +1,17 @@
+import { MongooseModule } from '@nestjs/mongoose';
 import { Test, TestingModule } from '@nestjs/testing';
+import { UserSchema } from './schemas/user.schema';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { config } from '../config/config';
 
 describe('UsersController', () => {
   let controller: UsersController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [MongooseModule.forFeature([{ name: 'User', schema: UserSchema}]),
+      MongooseModule.forRoot(config.mongoUri)],
       controllers: [UsersController],
       providers: [UsersService],
     }).compile();
